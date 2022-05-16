96f0e81b75ea1fd19cc8442144abb0cfcd80c858
Rienk <rienkfidder@gmail.com>
2022-05-16 12:30:11 +0200

    Formatting

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import networkx as nx
 
 def main():
     path_to_networks = ".\\data\\networks\\"
-    g = nx.read_edgelist(path_to_networks + "facebook_combined.txt", delimiter=' ', create_using=nx.Graph())
-
+    g = nx.read_edgelist(
+        path_to_networks + "facebook_combined.txt",
+        delimiter=" ",
+        create_using=nx.Graph(),
+    )
     print(nx.info(g))
 
 
